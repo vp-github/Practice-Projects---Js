b544ba86a9b31bc5b5fa04330684a700f37d0c17
Vishnupriya <vishnupriya@Vishnupriyas-MacBook-Air.local>
2022-01-17 15:33:51 +0530

    Music Player

---
+++ Music Player/js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Enna Solla Pogirai",
+    artist: "Shankar Mahadevan",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Nadhiye Nadhiye",
+    artist: "Unni Menon",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Snehidhane",
+    artist: "Sadhana Sargam",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Radhai Manadhil",
+    artist: "Chithra, Sujatha & Sangeetha",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Enakena Erkanave",
+    artist: "Unnikrishnan & Harini",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Mottu Ondru",
+    artist: "Hariharan & Sadhana Sargam",
+    img: "music-6",
+    src: "music-6",
+  },
+];
